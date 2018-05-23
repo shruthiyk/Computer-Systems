7cf419fcca0429c0f5f6f9f01aac01e3125193e0
Shruthi Kulkarni <shruthi.y.kulkarni@gmail.com>
2018-05-23 19:51:50 -0400

    Add files via upload

--- Assignment2/queue_array.c
+++ Assignment2/queue_array.c
@@ -27,7 +27,16 @@ typedef struct queue queue_t;
 // The queue should be initialized with data on
 // the heap.
 queue_t* create_queue(unsigned int _capacity){
+	
 	queue_t* myQueue = NULL;
+        int size;
+	
+	myQueue =(queue_t*)malloc(sizeof(queue_t));
+	myQueue->capacity = _capacity;
+	myQueue->size = size = 0 ;
+	myQueue->front = 0;
+	myQueue->back =-1;
+	myQueue->data = (int*)malloc(myQueue->capacity * sizeof(int));
 
 	return myQueue;
 }
@@ -36,18 +45,52 @@ queue_t* create_queue(unsigned int _capacity){
 // Check if the queue is empty
 // Returns 1 if true (The queue is completely empty)
 // Returns 0 if false (the queue has more space available)
-int queue_empty(queue_t* q){
+/*int queue_empty(queue_t* q){
+      
+
+     if (q->size == 0)
+	{       
+		return 1;
+	}
+	
+        else 
 
 	return 0;
+
+} */ 
+
+
+
+int queue_empty(queue_t* q){
+
+        if(q->size == 0)
+        {
+             printf(" queue is empty");
+             return 1;
+        }
+
+        else
+
+        return 0;
 }
+
 
 // Queue Full
 // Check if the queue is Full
 // Returns 1 if true (The queue is completely full)
 // Returns 0 if false (the queue has more space available)
 int queue_full(queue_t* q){
+	
+     if (q->size == q->capacity)
+	{
+	printf(" queue is full\n");
+	return 1;
+	}
+	
+	else
 
 	return 0;
+
 }
 
 // Enqueue a new item
@@ -55,15 +98,37 @@ int queue_full(queue_t* q){
 // Returns a -1 if the operation fails (otherwise returns 0 on success).
 // (i.e. the queue is full).
 int queue_enqueue(queue_t *q, int item){
-		return -1;
+	
+	if(!queue_full(q))              
+	{	
+	q->back = (q->back + 1)%q->capacity; 
+	q->data[q->back] = item;
+	q->size = q->size + 1;
+        printf(" added an item, %d\n" , item);
+	return 0;
+	}
+
+	else 
+ 	return -1;
+
 }
 
 // Dequeue an item
 // Returns the item at the front of the queue and
 // removes an item from the queue.
 int queue_dequeue(queue_t *q){
+	
+	if(queue_empty(q))
+	{
+	printf(" the queue is empty");
+	exit(EXIT_FAILURE);
+	}
+	
+	int item = q->data[q->front];
+	q->front = (q->front +1) % q->capacity;
+	q->size = q->size -1 ;
+	return item;
 
-		return -1;
 }
 
 
@@ -72,7 +137,11 @@ int queue_dequeue(queue_t *q){
 // A queue that has not been previously created will crash the program.
 // (i.e. A NULL q cannot return the size.)
 unsigned int queue_size(queue_t* q){
-	return 0;
+	
+        int queueSize = q->size; 
+        printf("\n queue size is %d\n " ,queueSize);
+	return queueSize;
+
 }
 
 
@@ -80,6 +149,10 @@ unsigned int queue_size(queue_t* q){
 // Removes a queue and all of its elements from memory.
 // This should be called before the proram terminates.
 void free_queue(queue_t* q){
+
+free(q->data);
+free(q);
+
 
 }
 
@@ -89,13 +162,34 @@ void free_queue(queue_t* q){
 // We will be adding our own to test your program.
 void unitTest1(){
 
-	queue_t* test1 = create_queue(1);
-	printf("Attempting to add %d\n",1);
+	queue_t* test1 = create_queue(5);
+	printf("Attempting to add %d\n",10);
+	queue_enqueue(test1,15);	
+	queue_enqueue(test1,3);	
+	queue_enqueue(test1,6);	
+	queue_size(test1);
 	queue_enqueue(test1,1);	
+	queue_enqueue(test1,1);	
+	queue_enqueue(test1,7);	
+	printf( " to check dequeue function\n");
 	printf("Removing: %d\n",queue_dequeue(test1));	
 
-	free_queue(test1);
+     	 
 }
+
+void unitTest2(){
+	queue_t* test2 = create_queue(2);
+	printf("Attempting to add %d\n",10);
+	queue_enqueue(test2,10);	
+	printf("Removing: %d\n",queue_dequeue(test2));	
+	queue_size(test2);
+
+	queue_dequeue(test2);
+	
+	free_queue(test2); 
+}
+
+
 
 
 // ====================================================
@@ -105,6 +199,10 @@ int main(){
 
 	// List of Unit Tests to test your data structure	
 	unitTest1();
-
+	unitTest2();
+	//queue_t* test2 = create_queue(2);
+	//queue_enqueue(test2,6);	
+	//queue_enqueue(test2,2);
+	
 	return 0;
 }
