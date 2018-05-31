2e31ea0f9c627f385f38d2271b74226c5748133a
shruthiyk <shruthi.y.kulkarni@gmail.com>
2018-05-31 15:47:19 -0400

    intial commit

--- Assignment3/sort.s
+++ Assignment3/sort.s
@@ -1,1 +1,119 @@
-Your code here
+.global _start
+
+.text
+
+sort:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movl	$0, -4(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L6:
+	movl	$0, -8(%rbp)
+	jmp	.L3
+.L5:
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %edx
+	movl	-8(%rbp), %eax
+	cltq
+	addq	$1, %rax
+	leaq	0(,%rax,4), %rcx
+	movq	-24(%rbp), %rax
+	addq	%rcx, %rax
+	movl	(%rax), %eax
+	cmpl	%eax, %edx
+	jle	.L4
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	movl	%eax, -12(%rbp)
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-8(%rbp), %eax
+	cltq
+	addq	$1, %rax
+	leaq	0(,%rax,4), %rcx
+	movq	-24(%rbp), %rax
+	addq	%rcx, %rax
+	movl	(%rax), %eax
+	movl	%eax, (%rdx)
+	movl	-8(%rbp), %eax
+	cltq
+	addq	$1, %rax
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-12(%rbp), %eax
+	movl	%eax, (%rdx)
+.L4:
+	addl	$1, -8(%rbp)
+.L3:
+	movl	-28(%rbp), %eax
+	subl	$1, %eax
+	cmpl	-8(%rbp), %eax
+	jg	.L5
+	addl	$1, -4(%rbp)
+.L2:
+	movl	-28(%rbp), %eax
+	subl	$1, %eax
+	cmpl	-4(%rbp), %eax
+	jg	.L6
+	nop
+	popq	%rbp
+	ret
+_start:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$48, %rsp
+	movl	$116, -48(%rbp)
+	movl	$67, -44(%rbp)
+	movl	$69, -40(%rbp)
+	movl	$102, -36(%rbp)
+	movl	$101, -32(%rbp)
+	movl	$102, -28(%rbp)
+	movl	$111, -24(%rbp)
+	movl	$114, -20(%rbp)
+	movl	$65, -16(%rbp)
+	leaq	-48(%rbp), %rax
+	movl	$9, %esi
+	movq	%rax, %rdi
+	call	sort
+	movl	$0, -4(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L8
+.L9:
+	leaq	-48(%rbp), %rax
+	movl	-4(%rbp), %edx
+	movslq	%edx, %rdx
+	salq	$2, %rdx
+	addq	%rdx, %rax
+	movl	$4, %edx
+	movq	%rax, %rsi
+	movl	$1, %edi
+	#call	write
+	mov 	$1, %rax
+	mov	$1, %rdi
+	mov 	$4, %rdx
+	syscall	
+	addl	$1, -4(%rbp)
+.L8:
+	cmpl	$8, -4(%rbp)
+	jle	.L9
+	movl	$0, %eax
+	leave
+	#ret
+	mov	$60, %rax
+	xor 	%rdi, %rdi
+	syscall
