56e899f65ccc6ed66a6faf7368da23cdd16ea904
shruthiyk <shruthi.y.kulkarni@gmail.com>
2018-06-13 18:25:15 -0400

    initial commit assignment 4

--- Assignment4_Shell/parse.c
+++ Assignment4_Shell/parse.c
@@ -1,1 +1,27 @@
 // Implement a working parser in this file that splits text into individual tokens.
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main()
+{
+
+char str[100]; // = "hello-hi-heu";
+printf(" Enter any string: ");
+scanf("%[^\n]%*c",str);
+char *token =  strtok(str,"-");
+
+	while(token!= NULL)
+	{	
+	printf("%s\n", token);
+	token = strtok(NULL,"-");
+	}
+
+return 0;
+
+}
+
+
+
+
