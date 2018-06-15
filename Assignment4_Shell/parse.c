2e5ba329f37aa17049f0f98485186b7475589ffe
shruthiyk <shruthi.y.kulkarni@gmail.com>
2018-06-15 00:58:20 -0400

    assignment 4

--- Assignment4_Shell/parse.c
+++ Assignment4_Shell/parse.c
@@ -4,24 +4,3 @@
 #include<string.h>
 #include<stdlib.h>
 
-int main()
-{
-
-char str[100]; // = "hello-hi-heu";
-printf(" Enter any string: ");
-scanf("%[^\n]%*c",str);
-char *token =  strtok(str,"|");
-
-	while(token!= NULL)
-	{	
-	printf("%s\n", token);
-	token = strtok(NULL,"-");
-	}
-
-return 0;
-
-}
-
-
-
-
--- Assignment4_Shell/shell.c
+++ Assignment4_Shell/shell.c
@@ -1,38 +1,190 @@
 // Modify this file for your assignment
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define READ_BUFFER_SIZE 80
+#define TOKEN_BUFFER_SIZE 64
+#define DELIMITERS "\t\n\a\r\v"
+
+
+// function to read line/command entered by the user
+
+char* read (void)
+{
+int buffer_size = READ_BUFFER_SIZE;
+int position = 0; 
+int character;
+char *r_buffer  = malloc(sizeof(char) * buffer_size);
+
+	if(!r_buffer)
+	{	
+		printf("memory allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	// loop through to read each character 
+	while(1)
+	{
+		character = getchar();
+
+		if( character == EOF || character == '\n')
+		{		
+
+		r_buffer[position] = '\0';
+		return r_buffer;
+		}
+
+		else {
+		r_buffer[position] = character ;
+		}
+ 	position++;
+
+
+
+	if(position >= buffer_size)
+	{
+	buffer_size+=READ_BUFFER_SIZE;
+	r_buffer = realloc(r_buffer, buffer_size);
+	if(!r_buffer)
+		{
+			printf(" memory allocation error\n");
+			exit(EXIT_FAILURE);	
+		}
+	}
+	}
+}
+
+
+// function to parse the line 
+
+char **parse( char* line)
+{
+int buffer_size = TOKEN_BUFFER_SIZE;
+int position = 0;
+char *token;
+char **t_size = malloc(buffer_size* sizeof(char*));
+
+	if(!token_size)
+	{
+	printf("memory allocation error\n");
+	exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, DELIMITERS);
+	while(token!=NULL)
+	{	
+	t_size[position] = token;
+	position++;
+
+	if(position >= buffer_size)
+        {
+        buffer_size+=TOKEN_BUFFER_SIZE;
+        t_buffer = realloc(t_buffer, buffer_size);
+        if(!t_buffer)
+                {
+                        printf(" memory allocation error\n");
+                        exit(EXIT_FAILURE);
+                }
+        }
+
+
+	token = strtok(NULL,TOKEN_BUFFER_SIZE);
+        }
+
+	t_size[position] = NULL ;
+	return t_size;
+}
+
+
+// functions to execute the command entered by the user 
+
+
+
+
+
+
+int execute(char **args)
+{
+
+pid_t pid;
+pid_w ;
+int status;
+
+
+pid= fork();
+	if(pid == 0)
+	{		
+	if(execvp(args[0],args) == -1)
+	{
+	perror("error");
+	}
+	exit(EXIT_FAILURE);
+	}  
+	else if (pid < 0)
+	{
+	perror("error");
+	}
+
+	else {
+ 
+	do {
+
+		w_pid = waitpid(pid, &status, WUNTRACED);
+	   }
+
+	while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	} 
+
+	return 1;
+}
+
+
+}
+}
+
+
+
+
 
 void loop()
 {
-
 char* line;
-char ** status;
-int status;
+char** args;
+int state;
 
 // read the commands entered by the user 
 // it will parse the commands computer can understand and execute the command
 
 do {
-printf("%s","mini-shell> ");
-line = read();
+	printf("mini-shell>");
+
+// read each line i.e the command that is typed in by the user
+
+	line = read_line();
 
 // agrs is an array of strings 
-args = parse(line);
 
-//  to execute and return the status
-status = execute(args);
+	args = parse(line);
+
+//to execute and return the state of the system
+
+	state =execute(args);
+
 // free the memory allocated 
-free(line);
-free(args);
 
-} while(status)
+	free(line);
+	free(args);
+
+} while(state);
 }
 
 
-int main (int agrc , char** argv){
+int main (int agrc , char** argv)
+{
 
-// load the configuration files
+// run an repl loop ie. read, evaluate, parse and loop again
+	loop();
 
-// run an repl loop: 
-loop();
-// perform any shutdown clean up
 return 0;
 }
