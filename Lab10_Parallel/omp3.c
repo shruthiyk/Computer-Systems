17b853a506dc2a804557b17844770204b1f800c0
Shruthi Kulkarni <shruthi.y.kulkarni@gmail.com>
2018-07-17 16:05:53 -0400

    Update omp3.c

--- Lab10_Parallel/omp3.c
+++ Lab10_Parallel/omp3.c
@@ -1,2 +1,28 @@
 // gcc -std=c99 -fopenmp omp3.c -o omp3
 // You fill in
+#include<stdio.h>
+#include<omp.h>
+
+
+// example for reduction clause 
+// to compute factorial 
+
+int factorial (int number)
+{
+
+int fac = 1; 
+#pragma omp parallel for reduction(*:fac)
+for( int n = 2;  n<= number; ++n)
+  	fac*=n;
+	return fac;
+}
+
+
+int main()
+{
+
+int number = factorial (4);
+printf("factorial is %d : ", number);
+return 0;
+
+}
