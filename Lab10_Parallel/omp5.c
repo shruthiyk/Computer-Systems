afc1f7edd814cd98f8253c28a957c2b42af1448a
Shruthi Kulkarni <shruthi.y.kulkarni@gmail.com>
2018-07-17 17:11:49 -0400

    Update omp5.c

--- Lab10_Parallel/omp5.c
+++ Lab10_Parallel/omp5.c
@@ -1,2 +1,20 @@
 // gcc -std=c99 -fopenmp omp5.c -o omp5
 // You fill in
+#include<stdio.h>
+#include<omp.h>
+
+// example to know data share between threads 
+int main()
+{
+
+int a, b= 0;
+#pragma omp parallel for private(a) shared(b)
+for(a = 0; a<50;++a)
+{
+ #pragma omp atomic 
+  b +=a;
+}
+
+printf (" b is %d\n", b);
+
+}
