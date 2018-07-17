be2717f17a20948f133d0508249ee3215c6cbdb6
Shruthi Kulkarni <shruthi.y.kulkarni@gmail.com>
2018-07-17 14:09:09 -0400

    Update omp1.c

--- Lab10_Parallel/omp1.c
+++ Lab10_Parallel/omp1.c
@@ -1,2 +1,17 @@
 // gcc -std=c99 -fopenmp omp1.c -o omp1
 // You fill in
+#include<omp.h>
+#include<stdio.h>
+
+int main()
+{
+	int threads = 50 ; 
+	int id ;
+	#pragma omp parallel 
+	{
+		threads = omp_get_num_threads();  // this gives the number of threads in the team
+		id = omp_get_thread_num();   // give the thread id 
+		printf (" this is thread %d from %d threads \n", id, threads);
+	}
+return 0;
+}
