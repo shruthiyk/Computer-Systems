76b7a9ea45962e90bc37d195ea78be4748db6e9b
Shruthi Kulkarni <shruthi.y.kulkarni@gmail.com>
2018-07-17 14:20:30 -0400

    Update omp2.c

--- Lab10_Parallel/omp2.c
+++ Lab10_Parallel/omp2.c
@@ -1,2 +1,27 @@
 // gcc -std=c99 -fopenmp omp2.c -o omp2
 // You fill in
+#include<stdio.h>
+#include<omp.h>
+
+
+int main()
+{
+
+	int threads = 50 ; 
+	int id ;
+
+	#pragma omp parallel 
+	{
+		threads = omp_get_num_threads();  // this gives the number of threads in the team
+		id = omp_get_thread_num();   // give the thread id 
+		
+		#pragma omp for 
+		for( int n=0 ; n<10 ; ++n )
+		{
+		printf (" thread id is %d \n", id);
+		}
+	
+	}
+
+return 0;
+}
